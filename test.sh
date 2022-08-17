67dcccdd8273e5c002f00a763eff1125bdf0c63c
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2022-08-17 10:52:40 +0900

    mul and div

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ assert() {
     expected="$1"
     input="$2"
 
-    ./boncc "$input" > tmp.s
+    ./boncc "$input" > tmp.s || exit $?
     cc -o tmp tmp.s
     ./tmp
     actual="$?"
@@ -21,5 +21,8 @@ assert 0 0
 assert 42 42
 assert 21 "5+20-4"
 assert 41 " 12 + 34 - 5 "
+assert 47 '5+6*7'
+assert 15 '5*(9-6)'
+assert 4 '(3+5)/2'
 
 echo OK

965b1be986f69bc13e816729aaf2823c26bc10c3
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2022-08-17 11:18:31 +0900

    implement unary

--- boncc.c
+++ boncc.c
@@ -138,6 +138,7 @@ Node *new_node_num(int val) {
 Node *expr();
 Node *mul();
 Node *primary();
+Node *unary();
 
 Node *expr() {
   Node *node = mul();
@@ -152,12 +153,12 @@ Node *expr() {
 }
 
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
   while (true) {
     if (consume('*'))
-      node = new_node(ND_MUL, node, mul());
+      node = new_node(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_node(ND_DIV, node, mul());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
@@ -171,6 +172,14 @@ Node *primary() {
   }
 
   return new_node_num(expect_number());
+}
+
+Node *unary() {
+  if (consume('+'))
+    return primary();
+  if (consume('-'))
+    return new_node(ND_SUB, new_node_num(0), primary());
+  return primary();
 }
 
 void gen(Node *node) {

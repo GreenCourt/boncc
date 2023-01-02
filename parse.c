778da539698b1418aeed927261e576647a6b8c25
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2023-01-02 00:24:45 +0000

    BNF at parse.c

--- parse.c
+++ parse.c
@@ -4,6 +4,41 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* BNF
+program    = toplevel*
+toplevel   = func | global
+type       = "int" "*"*
+global     = type ident ("[" num "]")* ";"
+global_init =
+func       = type ident "(" funcparam? ")" "{" stmt* "}"
+funcparam  = type ident ("[" num? "]")* ("," type ident ("[" num? "]")* )*
+stmt       = expr ";"
+             | type ident ("[" num "]")* ";"
+             | "{" stmt* "}"
+             | "if" "(" expr ")" stmt ("else" stmt)?
+             | "while" "(" expr ")" stmt
+             | "for" "(" expr? ";" expr? ";" expr? ")" stmt
+             | "return" expr ";"
+expr       = assign
+assign     = equality ("=" assign)?
+equality   = relational ("==" relational | "!=" relational)*
+relational = add ("<" add | "<=" add | ">" add | ">=" add)*
+add        = mul ("+" mul | "-" mul)*
+mul        = unary ("*" unary | "/" unary)*
+unary      = "sizeof"? unary
+             | "+"? unary
+             | "-"? unary
+             | "*"? unary
+             | "&"? unary
+             | postfix
+postfix    = primary ("[" expr "]")*
+primary    = "(" expr ")"
+             | ident ("(" (expr ("," expr)*)? ")")?
+             | ident
+             | num
+             | str
+*/
+
 typedef struct Scope Scope;
 struct Scope {
   Scope *prev;

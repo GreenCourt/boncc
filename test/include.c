8178fc7ed291f5eba34f73901a949ed965bc6aad
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2024-08-05 22:00:09 +0900

    add testcases

---
+++ test/include.c
@@ -0,0 +1,27 @@
+#include <assert.h>
+// #include <complex.h> // todo: _Complex
+#include <ctype.h>
+#include <errno.h>
+// #include <fenv.h> // todo: bit field
+#include <inttypes.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// #include <tgmath.h> // todo: requires complex.h
+#include <time.h>
+#include <uchar.h>
+#include <wchar.h>
+#include <wctype.h>
+
+// compiler-builtin headers
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+int main() { return 0; }

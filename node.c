41a69d6f9b49134e28d7579f8f7c60f967ba2dfd
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2024-08-10 12:42:37 +0900

    small fix

--- node.c
+++ node.c
@@ -84,13 +84,10 @@ Node *new_node_cast(Token *tok, Type *type, Node *operand) {
   assert(type->kind != TYPE_NONE);
   assert(operand->type->kind != TYPE_NONE);
 
-  if (!is_scalar(type) && type->kind != TYPE_VOID)
-    error(tok ? &tok->pos : NULL, "invalid type casting");
-
   if (same_type(type, operand->type))
     return operand;
 
-  if (is_struct_union(operand->type))
+  if (!is_scalar(type) && type->kind != TYPE_VOID)
     error(tok ? &tok->pos : NULL, "invalid type casting");
 
   if (operand->type->kind == TYPE_VOID && type->kind != TYPE_VOID)
